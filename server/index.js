6d3aab6cf10d4c36ff39ff539424002bb6a9888b
Ryan White <ryanjwhite10@gmail.com>
2018-07-20 18:43:04 +0000

    Refactor and remove unused server files

--- server/index.js
+++ server/index.js
@@ -5,9 +5,8 @@
 const PORT          = 8080;
 const express       = require("express");
 const bodyParser    = require("body-parser");
+const {MongoClient} = require('mongodb');
 const app           = express();
-//const {MongoClient} = require('mongodb');
-const MongoClient = require('mongodb').MongoClient;
 const MONGODB_URI   = 'mongodb://localhost:27017/tweeter';
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -21,15 +20,15 @@ MongoClient.connect(MONGODB_URI, (err, db) => {
 
   console.log(`Connected to mongodb: ${MONGODB_URI}`);
 
-  // Pass mongodb into DataHelpers to provide an interface to the tweets db
+  // Pass mongodb database into DataHelpers to provide an interface to the
+  // tweets db
   const DataHelpers = require("./lib/data-helpers.js")(db);
 
-  // Define routes that use DataHelpers interface to interact with the data
+  // Define routes that use DataHelpers interface to interact with the db
   const tweetsRoutes = require("./routes/tweets")(DataHelpers);
 
   // Mount the tweets routes at the "/tweets" path prefix:
   app.use("/tweets", tweetsRoutes);
-
 
   //db.close();
 });
