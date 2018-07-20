ae7b64713fa8f517f00decc0d2324d9f636bd808
Ryan White <ryanjwhite10@gmail.com>
2018-07-20 20:32:03 +0000

    Add /like endpoint and update likes in mongodb

--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -13,7 +13,19 @@ module.exports = function makeDataHelpers(db) {
         const sortNewestFirst = (a, b) => a.created_at - b.created_at;
         callback(null, tweets.sort(sortNewestFirst));
       });
+    },
+
+    changeTweetLikes: function(time, callback) {
+      // Find liked tweet based on timestamp and increment counter
+      db.collection('tweets').find().toArray((err, tweets) => {
+        let likedTweet = tweets.filter((tweet) => {
+          return tweet.created_at == time;
+        })[0];
+
+        db.collection('tweets').updateOne(likedTweet, {$inc: {likes: 1}});
+        callback();
+      });
     }
 
-  };
+  }
 }
--- server/routes/tweets.js
+++ server/routes/tweets.js
@@ -29,10 +29,23 @@ module.exports = function(DataHelpers) {
       content: {
         text: req.body.text
       },
-      created_at: Date.now()
+      created_at: Date.now(),
+      likes: 0
     };
 
     DataHelpers.saveTweet(tweet, (err) => {
+      if (err) {
+        res.status(500).json({ error: err.message });
+      } else {
+        res.status(201).send();
+      }
+    });
+  });
+
+  tweetsRoutes.post("/like", function(req, res) {
+    const time = req.body.created_at;
+
+    DataHelpers.changeTweetLikes(time, (err) => {
       if (err) {
         res.status(500).json({ error: err.message });
       } else {
